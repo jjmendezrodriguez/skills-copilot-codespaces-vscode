5949c15de20fb74a936d067cbded4f3efccae501
Jose J. Mendez Rodriguez <155632862+jjmendezrodriguez@users.noreply.github.com>
2024-10-28 03:42:26 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,37 +1,68 @@
-// create web server
-// 1. Load http module
-var http = require('http');
+// Create web server
+// Create a new comment
+// Get all comments
+
+// Require necessary modules
+var express = require('express');
+var bodyParser = require('body-parser');
 var fs = require('fs');
-var url = require('url');
 
-// 2. Create Server
-http.createServer(function (req, res) {
-  // Parse the request containing file name
-  var pathname = url.parse(req.url).pathname;
+// Create new web server
+var app = express();
 
-  // Print the name of the file for which request is made.
-  console.log("Request for " + pathname + " received.");
+// Use body-parser to parse JSON data
+app.use(bodyParser.json());
 
-  // Read the requested file content from file system
-  fs.readFile(pathname.substr(1), function (err, data) {
+// Create a new comment
+app.post('/comments', function(req, res) {
+  // Read the comments.json file
+  fs.readFile('comments.json', 'utf8', function(err, data) {
     if (err) {
-      console.log(err);
-      // HTTP Status: 404 : NOT FOUND
-      // Content Type: text/plain
-      res.writeHead(404, {'Content-Type': 'text/html'});
-    } else {
-      //Page found
-      // HTTP Status: 200 : OK
-      // Content Type: text/plain
-      res.writeHead(200, {'Content-Type': 'text/html'});
-
-      // Write the content of the file to response body
-      res.write(data.toString());
+      console.error(err);
+      process.exit(1);
     }
-    // Send the response body
-    res.end();
-  });
-}).listen(8081);
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+    // Parse the JSON data
+    var comments = JSON.parse(data);
+
+    // Add new comment
+    var newComment = {
+      id: Date.now(),
+      text: req.body.text
+    };
+    comments.push(newComment);
+
+    // Write the new comment to the comments.json file
+    fs.writeFile('comments.json', JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+
+      // Send the new comment as a response
+      res.json(newComment);
+    });
+  });
+});
+
+// Get all comments
+app.get('/comments', function(req, res) {
+  // Read the comments.json file
+  fs.readFile('comments.json', 'utf8', function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+
+    // Parse the JSON data
+    var comments = JSON.parse(data);
+
+    // Send the comments as a response
+    res.json(comments);
+  });
+});
+
+// Start the web server
+app.listen(3000, function() {
+  console.log('Server listening on port 3000');
+});
